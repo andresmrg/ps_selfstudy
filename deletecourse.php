1d475d09533d9b550d01df0b32d69a311215978c
andrewmrg <andresmao2@gmail.com>
2015-08-23 21:41:21 -0500

    Cleaned useless code

--- deletecourse.php
+++ deletecourse.php
@@ -3,21 +3,16 @@
 require_once('../../config.php');
 //require_once('deletecourse_form.php')
 
-global $DB, $OUTPUT, $PAGE;
+global $DB;
 
 //Get course ID
 $id = $_GET['id'];
 
-$context = context_system::instance();
-$PAGE->set_context($context);
-$PAGE->set_url('/blocks/ps_selfstudy/deletecourse.php');
-$PAGE->set_pagelayout('standard');
-
 //Delete course record
 if (!$DB->delete_records('block_ps_selfstudy_course', ['id' => $id])) {
-    print_error('inserterror', 'block_simplehtml');
+    print_error('inserterror', 'block_ps_selfstudy');
 }
-$courseurl = new moodle_url('/blocks/ps_selfstudy/managecourses.php');
-redirect($courseurl);
+$url = new moodle_url('/blocks/ps_selfstudy/managecourses.php');
+redirect($url);
 
 // form didn't validate or this is the first display
