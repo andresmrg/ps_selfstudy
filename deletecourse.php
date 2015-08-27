015e7214c0ef967b9a54e868964ad48e720b2a2f
andrewmrg <andrewramos@paradisosolutions.com>
2015-08-27 18:02:54 -0400

    Fix: Delete all records related to the course deleted, including requests and completion

--- deletecourse.php
+++ deletecourse.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+	Delete a course and delete all records related to the course (requests and completion)
+*/
+
 require_once('../../config.php');
 
 require_login();
@@ -11,11 +15,30 @@ global $DB;
 if(isset($_GET['id'])) {
 	$id = $_GET['id'];
 
+	//if the user has the right permissions
 	if (has_capability('block/ps_selfstudy:managecourses', $context, $USER->id)) {
-      //Delete course record
+      	//Delete course record
 		if (!$DB->delete_records('block_ps_selfstudy_course', ['id' => $id])) {
 			print_error('inserterror', 'block_ps_selfstudy');
 		}
+
+		//delete all requests related to this course if any
+		$requestlist = $DB->get_records('block_ps_selfstudy_request', array('course_id'=>$id), $sort='', $fields='id', $limitfrom=0, $limitnum=0);
+		if($requestlist) {
+			foreach($requestlist as $field) {
+				if (!$DB->delete_records('block_ps_selfstudy_request', array('id' => $requestlist->id)) {
+					print_error('inserterror', 'block_ps_selfstudy');
+				}
+				//Delete all completions with this requests if any
+				$completionid = $DB->get_record('block_ps_selfstudy_complete', array('request_id'=>$requestlist->id), $fields='id');
+				if($completionid) {
+					if (!$DB->delete_records('block_ps_selfstudy_complete', array('id' => $completionid->id)) {
+						print_error('inserterror', 'block_ps_selfstudy');
+					}
+				}
+			}
+		}
+		//Redirect to manage course page.
 		$url = new moodle_url('/blocks/ps_selfstudy/managecourses.php');
 		redirect($url);
 	} else {
