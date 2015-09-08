f9d23e8a3ef82fcd80741a7ce045a68f0cf256d2
andrewmrg <andrewramos@paradisosolutions.com>
2015-09-08 11:13:29 -0400

    New: Added filter by code in manage courses page

--- managecourses.php
+++ managecourses.php
@@ -7,6 +7,7 @@ require "../../config.php";
 require "$CFG->libdir/tablelib.php";
 require_once('managelinks_form.php');
 require "courselist_table.php";
+require_once('filter_form.php');
 global $OUTPUT, $PAGE;
 
 require_login();
@@ -20,32 +21,93 @@ $PAGE->set_url('/blocks/ps_selfstudy/managecourses.php');
 $PAGE->set_pagelayout('standard');
 $link_form = new managelinks_form();
 $table = new courselist_table('uniqueid');
+$filterform = new filter_form();
 
 // Define headers
 $PAGE->set_title(get_string('title_managecourses','block_ps_selfstudy'));
-$PAGE->set_heading('Manage self-study courses');
+$PAGE->set_heading(get_string('title_managecourses','block_ps_selfstudy'));
 
-$site = get_site();
-echo $OUTPUT->header(); //output header
-if (has_capability('block/ps_selfstudy:managecourses', $context, $USER->id)) {
+if($filterform->is_cancelled()) {
 
-	if(isset($_GET['success'])) {
-		$success = $_GET['success'];
-		if($success == 'del') {
-			echo '<div class="alert alert-success">'.get_string('course_deleted','block_ps_selfstudy').'</div>';	
-		} else {
-			echo '<div class="alert alert-success">'.get_string('course_edited','block_ps_selfstudy').'</div>';	
+	$courseurl = new moodle_url('/blocks/ps_selfstudy/managecourses.php');
+  	redirect($courseurl);
+
+} else if ($fromform = $filterform->get_data()) { 
+
+	$sqlconditions = "course_code = '".$fromform->filter_code."' AND ";
+
+	$site = get_site();
+	echo $OUTPUT->header(); //output header
+	
+	if (has_capability('block/ps_selfstudy:managecourses', $context, $USER->id)) {
+		//display message if any
+		if(isset($_GET['success'])) {
+			$success = $_GET['success'];
+			if($success == 'del') {
+				echo '<div class="alert alert-success">'.get_string('course_deleted','block_ps_selfstudy').'</div>';	
+			} else {
+				echo '<div class="alert alert-success">'.get_string('course_edited','block_ps_selfstudy').'</div>';	
+			}
 		}
+
+		//display filter form
+		$filterform->display();
+		echo "<hr>";
+
+		$link_form->display(); //output button to create new courses
+		// Get the course table.
+		//$table->set_sql('*', "{block_ps_selfstudy_course}", '1');
+
+		//sql to get all requests
+		$fields = '*';
+		$from = "{block_ps_selfstudy_course}";
+		$sqlconditions .= '1';
+		$table->set_sql($fields, $from, $sqlconditions);
+
+		$table->define_baseurl("$CFG->wwwroot/blocks/ps_selfstudy/managecourses.php");
+		$table->out(30, true); //print table
+	} else {
+		print_error('nopermissiontoviewpage', 'error', '');
 	}
-	$link_form->display(); //output button to create new courses
-	// Get the course table.
-	$table->set_sql('*', "{block_ps_selfstudy_course}", '1');
-	$table->define_baseurl("$CFG->wwwroot/blocks/ps_selfstudy/managecourses.php");
-	$table->out(30, true); //print table
+	echo $OUTPUT->footer();
+
 } else {
-	print_error('nopermissiontoviewpage', 'error', '');
+	
+	$site = get_site();
+	echo $OUTPUT->header(); //output header
+	
+	if (has_capability('block/ps_selfstudy:managecourses', $context, $USER->id)) {
+		//display message if any
+		if(isset($_GET['success'])) {
+			$success = $_GET['success'];
+			if($success == 'del') {
+				echo '<div class="alert alert-success">'.get_string('course_deleted','block_ps_selfstudy').'</div>';	
+			} else {
+				echo '<div class="alert alert-success">'.get_string('course_edited','block_ps_selfstudy').'</div>';	
+			}
+		}
+
+		//display filter form
+		$filterform->display();
+		echo "<hr>";
+
+		$link_form->display(); //output button to create new courses
+		// Get the course table.
+		//$table->set_sql('*', "{block_ps_selfstudy_course}", '1');
+
+		//sql to get all requests
+		$fields = '*';
+		$from = "{block_ps_selfstudy_course}";
+		$sqlconditions = '1';
+		$table->set_sql($fields, $from, $sqlconditions);
+
+		$table->define_baseurl("$CFG->wwwroot/blocks/ps_selfstudy/managecourses.php");
+		$table->out(30, true); //print table
+	} else {
+		print_error('nopermissiontoviewpage', 'error', '');
+	}
+	echo $OUTPUT->footer();
 }
-echo $OUTPUT->footer();
 echo "
 <script>
     function check_confirm()
