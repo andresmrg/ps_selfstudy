5a4774a5f1f6dbf27e152353585467f9a17c4b03
andrewmrg <andrewramos@paradisosolutions.com>
2015-09-09 11:23:55 -0400

    Change: Title and heading works with the lang file

--- viewallrequests.php
+++ viewallrequests.php
@@ -36,8 +36,8 @@ if($filterform->is_cancelled()) {
 	//WHEN FORM IS SUBMITTED
 	if (!$table->is_downloading()) {
 	//Define headers
-		$PAGE->set_title('View All Requests');
-		$PAGE->set_heading('View All Requests');
+		$PAGE->set_title(get_string('title_viewallrequests','block_ps_selfstudy'));
+		$PAGE->set_heading(get_string('title_viewallrequests','block_ps_selfstudy'));
 		$site = get_site();
 		echo $OUTPUT->header(); //output header
 		$filterform->display();
@@ -70,8 +70,8 @@ if($filterform->is_cancelled()) {
 	//FIRST TIME
 	if (!$table->is_downloading()) {
 	//Define headers
-		$PAGE->set_title('View All Requests');
-		$PAGE->set_heading('View All Requests');
+		$PAGE->set_title(get_string('title_viewallrequests','block_ps_selfstudy'));
+		$PAGE->set_heading(get_string('title_viewallrequests','block_ps_selfstudy'));
 		$site = get_site();
 		echo $OUTPUT->header(); //output header
 		$filterform->display();
