6ec12fbd87d2b3a8271161f5247d340590f449f0
andrewmrg <andrewramos@paradisosolutions.com>
2015-09-25 09:44:00 -0400

    Removed myaddinstance permission

--- db/access.php
+++ db/access.php
@@ -1,16 +1,6 @@
 <?php
     $capabilities = array(
- 
-    'block/ps_selfstudy:myaddinstance' => array(
-        'captype' => 'write',
-        'contextlevel' => CONTEXT_SYSTEM,
-        'archetypes' => array(
-            'user' => CAP_ALLOW
-        ),
- 
-        'clonepermissionsfrom' => 'moodle/my:manageblocks'
-    ),
- 
+  
     'block/ps_selfstudy:addinstance' => array(
         'riskbitmask' => RISK_SPAM | RISK_XSS,
  
