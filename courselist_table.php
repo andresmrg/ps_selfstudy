1297b7105ef8ef8c35f47347c0b608e9d3d62036
andrewmrg <andrewramos@paradisosolutions.com>
2015-09-25 16:01:39 -0400

    Fix the URl on the course code

--- courselist_table.php
+++ courselist_table.php
@@ -42,6 +42,11 @@ class courselist_table extends table_sql {
 
         global $DB;
         $desc_link = $DB->get_record('block_ps_selfstudy_course',array('id'=>$values->id), $fields='description_link');
+        $url = $desc_link->description_link;
+        
+        if (!preg_match("~^(?:f|ht)tps?://~i", $url)) {
+            $url = "http://" . $url;
+        }
 
         if(!empty($desc_link) && $desc_link->description_link !== NULL && $desc_link->description_link !== "") {
             return '<a href="'.$desc_link->description_link.'" target="_blank">'.$values->course_code.'</a>';
